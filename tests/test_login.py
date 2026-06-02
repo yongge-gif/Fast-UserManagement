7b44f2f54c7ab0ea452871819f4a562361fdf94c
yongge-gif <1537546223@qq.com>
2026-06-02 13:50:14 +0800

    登录测试里自己先注册用户

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,4 @@
+import uuid
 from fastapi.testclient import TestClient
 from main import app
 
@@ -6,11 +7,23 @@ client = TestClient(app)
 
 # 登录测试接口
 def test_login():
+    username = f"pytest_{uuid.uuid4().hex[:8]}"
 
+    # 先注册
+    client.post(
+        "/register",
+        data={
+            "username": username,
+            "password": "123456",
+            "email": f"{username}@test.com"
+        }
+    )
+
+    # 再登录
     response = client.post(
         "/login",
         json={
-            "username": "pytest_user",
+            "username": username,
             "password": "123456"
         }
     )
